fb217858893a02b72b9153855d732cd99a7aff73
Jaime Jeovanny Cortez Flores <jaimecortez@ufg.edu.sv>
2025-09-18 20:04:06 -0600

    PRACTICA DEL 18 DE SEPTIEMBRE DEL 2025

---
+++ classes/estudiante.class.php
@@ -0,0 +1,100 @@
+<?php
+require("classes/conn.class.php");
+require("classes/validaciones.inc.php");
+
+class Estudiante{
+    public $idestudiante;
+    public $fechanacimiento;
+    public $estadoregistroestudiante;
+    public $idgenero;
+    public $conexion;
+    public $validacion;
+
+    public function __construct(){
+        $this->conexion = new DB();
+        $this->validacion = new Validaciones();
+    }
+
+    ///SETTER DE LA VARIAABLE idestudiante
+    public function setIdEstudiante($idestudiante){
+        $this->idestudiante = $idestudiante;
+    }
+
+    //GETTER DE LA VARIABLE idestudiante
+    public function getIdEstudiante(){
+        return $this->idestudiante;
+    }
+
+    public function setFechaNacimiento($fechanacimiento){
+        $this->fechanacimiento = $fechanacimiento;
+    }
+
+    public function getFechaNacimiento(){
+        return $this->fechanacimiento;
+    }
+
+    public function setEstadoRegistroEstudiante($estadoregistroestudiante){
+        $this->estadoregistroestudiante = $estadoregistroestudiante;
+    }
+
+    public function getEstadoRegistroEstudiante(){
+        return $this->estadoregistroestudiante;
+    }
+
+    public function setIdGenero($idgenero){
+        $this->idgenero = $idgenero;
+    }
+
+    public function getIdGenero(){
+        return $this->idgenero;
+    }
+
+
+    //METODO PARA OBTENER TODOS LOS ESTUDIANTES
+    public function obtenerEstudiantes(){
+        $resultado = $this->conexion->run('SELECT * FROM estudiante;');
+        $array = array("mensaje"=>"Registros encontrados","data"=>$resultado->fetchAll());
+        return $array;
+    }
+
+    //METODO PARA OBTENER UN ESTUDIANTE
+    public function obtenerEstudiante(int $idestudiante){
+        if($idestudiante > 0){
+            $resultado = $this->conexion->run('SELECT * FROM estudiante WHERE id_estudiante='.$idestudiante);
+            $array = array("mensaje"=>"Registros encontrados","data"=>$resultado->fetch());
+            return $array;
+        }else{
+            $array = array("mensaje"=>"Registros NO encontrados, identificador incorrecto","data"=>"");
+            return $array;
+        }
+    }
+
+
+    public function nuevoEstudiante($fechanacimiento,$idgenero){
+        if(!empty($idgenero) && !empty($fechanacimiento)){
+            //VARIABLE DE TIPO ARRAY PARA ENVIAR PARÁMETROS A LA BASE DE DATOS
+            $parametros = array(
+                "fecha_nac" => $fechanacimiento,
+                "id_genero" => $idgenero
+            );
+
+            $resultado = $this->conexion->run('INSERT INTO estudiante(fecha_nacimiento_estudiante,id_genero)VALUES(:fecha_nac,:id_genero);',$parametros);
+            if($this->conexion->n > 0 and $this->conexion->id > 0){
+                $resultado = $this->obtenerEstudiante($this->conexion->id);
+                $array = array("mensaje"=>"Registros encontrados","data"=>$resultado["data"]);
+                return $array;
+            }else{
+                $array = array("mensaje"=>"Hubo un problema al registrar el estudiante","data"=>"");
+                return $array;
+            }
+        }else{
+            $array = array("mensaje"=>"Parámetros enviados vacíos","data"=>"");
+            return $array;
+        }
+    }
+
+
+
+}
+
+?>
---
+++ process.php
@@ -0,0 +1,10 @@
+<?php
+require("classes/estudiante.class.php");
+$Estudiante = new Estudiante();
+
+$resultado = $Estudiante->obtenerEstudiantes();
+
+header("Content-Type: Application/json");
+echo(json_encode($resultado));
+
+?>
